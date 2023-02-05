0e944ecf91d9dd6ed553bee725fbe3535233b5fe
Lib0823 <robot082373@naver.com>
2023-02-05 15:47:24 +0900

    feat: JUnit 실습 코드 작성

---
+++ src/main/java/com/example/springcalculator/component/ICalculator.java
@@ -0,0 +1,15 @@
+package com.example.springcalculator.component;
+
+public interface ICalculator {
+
+    int sum(int x, int y);
+
+    int minus(int x, int y);
+
+    int multiply(int x, int y);
+
+    int division(int x, int y);
+
+    void init();
+
+}
